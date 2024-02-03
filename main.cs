7506fc8af4d399371547c31440e18d258fa6a30c
Chromwell <31803618-Chromwell@users.noreply.replit.com>
2024-02-03 02:49:58 +0000

    Age was added

--- main.cs
+++ main.cs
@@ -4,6 +4,7 @@ class Program {
   public static void Main (string[] args) {
     string FirstName = "Shinji";
     string LastName = "Monton";
-    Console.WriteLine ("Hello World I am " + FirstName + " " +LastName);
+    int age = 20;
+    Console.WriteLine ("Hello World I am " + FirstName + " " + LastName + " " + age);
   }
 }
